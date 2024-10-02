ade049ae4e1074032e78d90e60b5b690452f8ce2
charinstefany16 <charinstefany@gmail.com>
2024-10-02 22:58:30 +0700

    Add files via upload

---
+++ Praktikum algo 2.py
@@ -0,0 +1,13 @@
+import math
+Lintang1 = math.radians(float(input("Lintang Kota 1: ")))
+Bujur1 = math.radians(float(input("Bujur Kota 1: ")))
+Lintang2 = math.radians(float(input("Lintang kota 2: ")))
+Bujur2 = math.radians(float(input("Bujur kota 2: ")))
+R = 6371
+Lat = Lintang2 - Lintang1
+Long = Bujur2 - Bujur1
+
+a = math.sin(Lat/2)*2 + math.cos(Lintang1) * math.cos(Lintang2) * math.sin(Long/2)*2
+C3 = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+d = R * C3
+print(f"Jarak antara dua titik tersebut adalah {d} kilometer.")
